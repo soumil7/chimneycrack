a9349dcf840f152ca184c3e5623252b71f4e7ec7
soumil7 <121105628+soumil7@users.noreply.github.com>
2025-06-14 18:57:38 +0530

    Update app.py port number

--- app.py
+++ app.py
@@ -23,4 +23,5 @@ def predict():
         return jsonify({'status': 'Error', 'message': e})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
